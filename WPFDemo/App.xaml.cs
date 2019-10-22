64865111e46e1b0572e8160f2e75a2ba249cd018
Leon <leon.id@qq.com>
2019-10-22 14:59:08 +0800

    修改 MainWindow 继承自 NavigationWindow；

--- WPFDemo/App.xaml.cs
+++ WPFDemo/App.xaml.cs
@@ -13,5 +13,41 @@ namespace WPFDemo
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 构造 App，相当于程序入口
+        /// </summary>
+        public App()
+        {
+            this.Startup += this.App_Startup;
+            this.Activated += this.App_Activated;
+            this.Exit += this.App_Exit;
+            this.DispatcherUnhandledException += this.App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += this.CurrentDomain_UnhandledException;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        private void App_Exit(object sender, ExitEventArgs e)
+        {
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+        }
+
+        private void App_Activated(object sender, EventArgs e)
+        {
+        }
     }
 }
