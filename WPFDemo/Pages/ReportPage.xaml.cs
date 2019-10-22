f4e1acace411ac5e98b5b358e0077bea1c341ad5
Leon <Leon.ID@QQ.COM>
2019-10-22 22:10:35 +0800

    ReportPage 使用 DataContext 绑定数据；

--- WPFDemo/Pages/ReportPage.xaml.cs
+++ WPFDemo/Pages/ReportPage.xaml.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace WPFDemo.Pages
 {
@@ -20,7 +9,13 @@ namespace WPFDemo.Pages
     {
         public ReportPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        public ReportPage(object data) : this()
+        {
+            // 使用 DataContext 绑定数据
+            this.DataContext = data;
         }
     }
 }
