9c66f49b712dfbd454fbb95812cada52bd31fb38
Leon <leon.id@qq.com>
2019-10-29 19:15:01 +0800

    增加无边框按钮模板；

---
+++ NoneBorderResizableWindows/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace NoneBorderResizableWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NoneButton_Click(object sender, RoutedEventArgs e)
+        {
+            var noneWindow = new NoneWindow();
+            noneWindow.Show();
+        }
+
+        private void BorderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
