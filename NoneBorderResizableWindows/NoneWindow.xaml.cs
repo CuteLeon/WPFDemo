b5e2b5b0c7d7892c3a644d90441dd53fb61a5b2b
Leon <leon.id@qq.com>
2019-10-30 17:48:19 +0800

    <x:Code> 代码前置，需要使用 <![CDATA[]]> 转移特殊字符，前置的代码将无法断点调试；

--- NoneBorderResizableWindows/NoneWindow.xaml.cs
+++ NoneBorderResizableWindows/NoneWindow.xaml.cs
@@ -12,26 +12,5 @@ namespace NoneBorderResizableWindows
         {
             this.InitializeComponent();
         }
-
-        private void TitleGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            // 鼠标左键拖动标题栏
-            this.DragMove();
-        }
-
-        private void MinButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void MaxRestoreButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
     }
 }
