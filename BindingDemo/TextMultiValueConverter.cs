fd916da3a56818db1a02bb7c7b257ce5faf6a6e9
Leon <Leon.ID@QQ.COM>
2019-11-02 20:03:14 +0800

    MultiBinding IMultiValueConverter 多数据源绑定几转换器；

---
+++ BindingDemo/TextMultiValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace BindingDemo
+{
+    public class TextMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Cast<string>().All(value => !string.IsNullOrEmpty(value));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
