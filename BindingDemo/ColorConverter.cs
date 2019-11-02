3c374d84e51b88cd6e3fc34d2f9c5c2bb27d0a37
Leon <Leon.ID@QQ.COM>
2019-11-02 19:46:39 +0800

    Binding IValueConverter 值转换器；

---
+++ BindingDemo/ColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using media = System.Windows.Media;
+using draw = System.Drawing;
+
+namespace BindingDemo
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var drawColor = draw.Color.FromName(value as string);
+            var mediaColor = new media.Color() { A = 255, R = drawColor.R, G = drawColor.G, B = drawColor.B };
+            return new media.SolidColorBrush(mediaColor);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((media.Color)value).ToString();
+        }
+    }
+}
