9fd000d7eff1a87f84b17dfef023578522304ffe
Leon <Leon.ID@QQ.COM>
2019-11-03 18:26:49 +0800

    完成事件冒泡、隧道、直达模式；

---
+++ EventDemo/LeonButton.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EventDemo
+{
+    /// <summary>
+    /// LeonButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class LeonButton : UserControl
+    {
+        /// <summary>
+        /// 冒泡点击事件
+        /// </summary>
+        public static readonly RoutedEvent ClickBubbleEvent = EventManager.RegisterRoutedEvent("ClickBubble", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(LeonButton));
+
+        /// <summary>
+        /// 隧道点击事件
+        /// </summary>
+        public static readonly RoutedEvent ClickTunnelEvent = EventManager.RegisterRoutedEvent("ClickTunnel", RoutingStrategy.Tunnel, typeof(RoutedEventHandler), typeof(LeonButton));
+
+        /// <summary>
+        /// 直达点击事件
+        /// </summary>
+        public static readonly RoutedEvent ClickDirectEvent = EventManager.RegisterRoutedEvent("ClickDirect", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(LeonButton));
+
+        public event RoutedEventHandler ClickBubble
+        {
+            add { this.AddHandler(ClickBubbleEvent, value); }
+            remove { this.RemoveHandler(ClickBubbleEvent, value); }
+        }
+
+        public event RoutedEventHandler ClickTunnel
+        {
+            add { this.AddHandler(ClickTunnelEvent, value); }
+            remove { this.RemoveHandler(ClickTunnelEvent, value); }
+        }
+
+        public event RoutedEventHandler ClickDirect
+        {
+            add { this.AddHandler(ClickDirectEvent, value); }
+            remove { this.RemoveHandler(ClickDirectEvent, value); }
+        }
+
+        public LeonButton() : base()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonUp(e);
+
+            this.RaiseEvent(new RoutedEventArgs(ClickBubbleEvent, this));
+            // this.RaiseEvent(new RoutedEventArgs(ClickDirectEvent, this));
+        }
+
+        protected override void OnMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseRightButtonUp(e);
+
+            this.RaiseEvent(new RoutedEventArgs(ClickTunnelEvent, this));
+            // this.RaiseEvent(new RoutedEventArgs(ClickDirectEvent, this));
+        }
+    }
+}
--- EventDemo/MainWindow.xaml.cs
+++ EventDemo/MainWindow.xaml.cs
@@ -14,13 +14,19 @@ namespace EventDemo
         {
             this.InitializeComponent();
 
+            /* RoutingStrategy 路由策略
+             * Bubble : 冒泡，事件由源头沿着唯一明确的路径层层向上传递，直到处理或到达顶级；
+             * Tunnel : 隧道，方向与 Bubble 相反，但路径相同，事件由顶级向源头传播；
+             * Direct : 直达，原生的事件订阅模式；
+             */
+
             // 使用后台代码监听路由事件
             this.gridRoot.AddHandler(Button.ClickEvent, new RoutedEventHandler(this.CSharp_Grid_Click));
         }
 
         private void XAML_Grid_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"XAML 绑定的路由事件：\n接收者：{(sender as Grid).Name}\n事件源头：{(e.OriginalSource as Button).Content}\n事件名称：{e.RoutedEvent.Name}");
+            MessageBox.Show($"XAML 绑定的路由事件：\n接收者：{(sender as Grid).Name}\n事件源头：{(e.OriginalSource as Button).Content}\n事件名称：{e.RoutedEvent.Name}\n事件策略：{e.RoutedEvent.RoutingStrategy}");
 
             if (new Random().NextDouble() > 0.5)
             {
@@ -31,12 +37,17 @@ namespace EventDemo
 
         private void CSharp_Grid_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"CSharp 绑定的路由事件：\n接收者：{(sender as Grid).Name}\n事件源头：{(e.OriginalSource as Button).Content}\n事件名称：{e.RoutedEvent.Name}");
+            MessageBox.Show($"CSharp 绑定的路由事件：\n接收者：{(sender as Grid).Name}\n事件源头：{(e.OriginalSource as Button).Content}\n事件名称：{e.RoutedEvent.Name}\n事件策略：{e.RoutedEvent.RoutingStrategy}");
         }
 
         private void gridMiddle_Click(object sender, RoutedEventArgs e)
         {
             Debug.Print($"{(e.OriginalSource as Button).Content} 的事件经过了 {(sender as Grid).Name}");
+        }
+
+        private void LeonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.Print($"{(sender as LeonButton).Tag} 收到了 {(e.OriginalSource as LeonButton).Tag} 的路由事件：{e.RoutedEvent.Name} ({e.RoutedEvent.RoutingStrategy.ToString()})");
         }
     }
 }
