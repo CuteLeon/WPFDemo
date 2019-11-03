b8b180755211cc3bf5701cfadfcb59a3f6172b93
Leon <Leon.ID@QQ.COM>
2019-11-03 16:16:51 +0800

    增加依赖属性 DependencyProperty 演示；

---
+++ DependencyPropertyDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace DependencyPropertyDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            var school = new School() { SchoolName = "测试学校" };
+            this.DataContext = school;
+
+            // 两种方式绑定 School.SchoolNameProperty
+            BindingOperations.SetBinding(
+                this.schoolNameTextBox1,
+                TextBox.TextProperty,
+                new Binding(nameof(School.SchoolName))
+                {
+                    Source = school,
+                    Mode = BindingMode.TwoWay,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                });
+
+            BindingOperations.SetBinding(
+                school,
+                School.SchoolNameProperty,
+                new Binding(nameof(TextBox.Text))
+                {
+                    Source = this.schoolNameTextBox2,
+                    Mode = BindingMode.TwoWay,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                });
+        }
+    }
+}
---
+++ DependencyPropertyDemo/School.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace DependencyPropertyDemo
+{
+    // 依赖属性（DependencyProperty）需要依赖对象（DependencyObject）
+    public class School : DependencyObject
+    {
+        /// <summary>
+        /// 学校名称（CLR 属性）
+        /// </summary>
+        /// <remarks>手动封装一个 CLR 属性</remarks>
+        public string SchoolName
+        {
+            get => (string)this.GetValue(School.SchoolNameProperty);
+            set => this.SetValue(School.SchoolNameProperty, value);
+        }
+
+        /// <summary>
+        /// 依赖属性
+        /// </summary>
+        /// <remarks>
+        /// 代码片段名称：propa
+        /// 需要修饰：public static readonly
+        /// 数据结构：DependencyObject 存在一个静态 HashTable 类型，用于存储依赖属性的数据（使用组合模式，节省大量非必要依赖属性数据的内存）
+        /// </remarks>
+        public static readonly DependencyProperty SchoolNameProperty =
+            DependencyProperty.RegisterAttached(
+                nameof(School.SchoolName),
+                typeof(string),
+                typeof(School),
+                new PropertyMetadata("默认值"));
+    }
+}
