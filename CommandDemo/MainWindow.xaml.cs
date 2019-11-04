1f0811e9ab6139f32ac4f53dac80bdaef4d92ed9
Leon <leon.id@qq.com>
2019-11-04 18:48:52 +0800

    增加命令演示；

---
+++ CommandDemo/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace CommandDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public RoutedCommand SubmitCommand = new RoutedCommand("Submit", typeof(MainWindow));
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.InitializeCommand();
+        }
+
+        private void InitializeCommand()
+        {
+            this.submitButton.Command = this.SubmitCommand;
+
+            // 绑定快捷键
+            this.SubmitCommand.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            // 绑定命令目标
+            this.submitButton.CommandTarget = this.messageTextBox;
+
+            // 增加命令绑定
+            var commandBinding = new CommandBinding() { Command = this.SubmitCommand };
+
+            /* PreviewCanExecute 和 CanExecute 事件由框架自动触发，且频率较高，会对性能造成影响，使用时务必谨慎
+            // 是否允许执行前事件
+            commandBinding.PreviewCanExecute += (s, e) => { e.Handled = true; };
+            // 执行前事件
+            commandBinding.PreviewExecuted += (s, e) => { e.Handled = true; };
+             */
+            // 是否允许执行事件（与事件源的 Enabled 属性关联）
+            commandBinding.CanExecute += this.CommandBinding_CanExecute;
+            // 执行后事件
+            commandBinding.Executed += this.CommandBinding_Executed;
+
+            // 将命令绑定添加到外围容器控件
+            this.dockPanel.CommandBindings.Add(commandBinding);
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(this.messageTextBox.Text);
+            e.Handled = true;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show($"发送消息：\n{this.messageTextBox.Text}");
+            this.messageTextBox.Text = string.Empty;
+            e.Handled = true;
+        }
+    }
+}
