cdd6eb40e1d4e9f571050eec93c06821f2bc92aa
Leon <leon.id@qq.com>
2019-11-05 15:57:14 +0800

    使用 Source 引入外部的资源字典文件

--- ResourceDemo/MainWindow.xaml.cs
+++ ResourceDemo/MainWindow.xaml.cs
@@ -14,7 +14,10 @@ namespace ResourceDemo
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // 自动查找本身元素的资源，不存在时向上层元素查找资源
             var title = (string)this.FindResource("title");
+
+            // 如果明确知道资源所在的元素，可以通过Resources字典直接读写；
             this.Resources["title"] = "改变了 title 资源数据";
         }
     }
